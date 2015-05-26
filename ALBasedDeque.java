55cc59db6b81a65cdc8db9cbed49f2047ceef91c
asiansies <aihoale@bu.edu>
2015-05-25 22:04:50 -0400

    Add ALBasedDeque.java

---
+++ ALBasedDeque.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+ 
+public class ALBasedDeque {
+ 
+    private ArrayList dequedata = new ArrayList();
+     
+    public int size() {
+        return dequedata.size();
+    }
+     
+    public boolean isEmpty() {
+        return dequedata.isEmpty();
+    }
+     
+    public T getFirst(T value) throws EmptyDequeException {
+         
+    }
+     
+     
+}
